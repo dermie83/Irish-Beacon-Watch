e3a76f51b7f55f40801d9d5dbf13e72c7bcdda6c
dermie83 <dermiemadsen@gmail.com>
2025-02-03 17:25:17 +0000

    initial commit from Dashboard Boiler Plate

---
+++ CHANGELOG.md
@@ -0,0 +1,17 @@
+# Irish Beacon Watch Changelog
+
+## verson 0.1.1
+Added map to home page
+Added weather api data forecast page (graphs and tables are still to be decided on)
+
+## verson 0.1.0
+Clone example boiler plate from nextjs official web page https://nextjs.org/learn/dashboard-app/getting-started
+Run pnpm i to install the project's packages.
+Run pnpm dev
+Push Project to Git Repository
+Pushed project from git repo to Vercel account for deployement
+Created a new connection to Neon PostresSQL database
+added .env file
+added database credentials to .env and executed pnpm i @vercel/postgres to retrieve database dependencies
+Seeded database with lighthouse table
+Changed page names from Dashboard project to Beacon watch project
--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,218 +1,107 @@
-import postgres from 'postgres';
+import axios from "axios";
+import { sql } from '@vercel/postgres';
 import {
-  CustomerField,
-  CustomersTableType,
-  InvoiceForm,
-  InvoicesTable,
-  LatestInvoiceRaw,
-  Revenue,
-} from './definitions';
-import { formatCurrency } from './utils';
+  CurrentWeatherType, 
+  DailytWeatherType, 
+  HourlyWeatherType, 
+  LighthouseProps
+      } from './definitions';
+// import { formatCurrency } from './utils';
 
-const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
-
-export async function fetchRevenue() {
+export async function fetchLighthouse() {
   try {
-    // Artificially delay a response for demo purposes.
-    // Don't do this in production :)
-
-    // console.log('Fetching revenue data...');
-    // await new Promise((resolve) => setTimeout(resolve, 3000));
-
-    const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
-    // console.log('Data fetch completed after 3 seconds.');
-
-    return data;
+    const data = await sql<LighthouseProps>`SELECT * FROM lighthouse`;
+    return data.rows;
   } catch (error) {
     console.error('Database Error:', error);
     throw new Error('Failed to fetch revenue data.');
   }
 }
 
-export async function fetchLatestInvoices() {
-  try {
-    const data = await sql<LatestInvoiceRaw[]>`
-      SELECT invoices.amount, customers.name, customers.image_url, customers.email, invoices.id
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      ORDER BY invoices.date DESC
-      LIMIT 5`;
 
-    const latestInvoices = data.map((invoice) => ({
-      ...invoice,
-      amount: formatCurrency(invoice.amount),
-    }));
-    return latestInvoices;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch the latest invoices.');
+export async function getWeather(
+    lat: number,
+    lon: number,
+    timezone: string
+    // Promise<{current:object, daily:object, hourly:object}> {
+  ): Promise<{
+    current: CurrentWeatherType;
+    daily: DailytWeatherType[];
+    hourly: HourlyWeatherType[];
+  }> {
+  
+    return await axios
+      .get(
+        "https://api.open-meteo.com/v1/forecast?current=temperature_2m,weather_code,wind_speed_10m&hourly=temperature_2m,apparent_temperature,precipitation,weather_code,wind_speed_10m&daily=weather_code,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&precipitation_unit=inch&timeformat=unixtime",
+        {
+          params: {
+            latitude: lat,
+            longitude: lon,
+            timezone,
+          },
+        }
+      )
+      .then((response:any) => {
+        //OR then(({data}))
+        return {
+          current: parseCurrentWeather(response.data),
+          daily: parseDailyWeather(response.data),
+          hourly: parseHourlyWeather(response.data),
+        };
+      });
+  
+    // return resp.data;
   }
-}
 
-export async function fetchCardData() {
-  try {
-    // You can probably combine these into a single SQL query
-    // However, we are intentionally splitting them to demonstrate
-    // how to initialize multiple queries in parallel with JS.
-    const invoiceCountPromise = sql`SELECT COUNT(*) FROM invoices`;
-    const customerCountPromise = sql`SELECT COUNT(*) FROM customers`;
-    const invoiceStatusPromise = sql`SELECT
-         SUM(CASE WHEN status = 'paid' THEN amount ELSE 0 END) AS "paid",
-         SUM(CASE WHEN status = 'pending' THEN amount ELSE 0 END) AS "pending"
-         FROM invoices`;
-
-    const data = await Promise.all([
-      invoiceCountPromise,
-      customerCountPromise,
-      invoiceStatusPromise,
-    ]);
-
-    const numberOfInvoices = Number(data[0].count ?? '0');
-    const numberOfCustomers = Number(data[1].count ?? '0');
-    const totalPaidInvoices = formatCurrency(data[2][0].paid ?? '0');
-    const totalPendingInvoices = formatCurrency(data[2][0].pending ?? '0');
-
+  function parseCurrentWeather({ current, daily }: any): CurrentWeatherType {
+    const {
+      temperature_2m: currentTemp,
+      wind_speed_10m: windSpeed,
+      weather_code: iconCode,
+    } = current;
+    const {
+      temperature_2m_max: [maxTemp],
+      temperature_2m_min: [minTemp],
+      apparent_temperature_max: [maxFeelsLike],
+      apparent_temperature_min: [minFeelsLike],
+      precipitation_sum: [precip],
+    } = daily;
+  
+    // temperature_2m: [maxTemp] same as const maxTemp = daily.temperature_2m[0]
     return {
-      numberOfCustomers,
-      numberOfInvoices,
-      totalPaidInvoices,
-      totalPendingInvoices,
+      currentTemp: Math.round(currentTemp),
+      highTemp: Math.round(maxTemp),
+      lowTemp: Math.round(minTemp),
+      highFeelsLike: Math.round(maxFeelsLike),
+      lowFeelsLike: Math.round(minFeelsLike),
+      windSpeed: Math.round(windSpeed),
+      precip: Math.round(precip * 100) / 100,
+      iconCode: iconCode,
     };
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch card data.');
   }
-}
-
-const ITEMS_PER_PAGE = 6;
-export async function fetchFilteredInvoices(
-  query: string,
-  currentPage: number,
-) {
-  const offset = (currentPage - 1) * ITEMS_PER_PAGE;
-
-  try {
-    const invoices = await sql<InvoicesTable[]>`
-      SELECT
-        invoices.id,
-        invoices.amount,
-        invoices.date,
-        invoices.status,
-        customers.name,
-        customers.email,
-        customers.image_url
-      FROM invoices
-      JOIN customers ON invoices.customer_id = customers.id
-      WHERE
-        customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`} OR
-        invoices.amount::text ILIKE ${`%${query}%`} OR
-        invoices.date::text ILIKE ${`%${query}%`} OR
-        invoices.status ILIKE ${`%${query}%`}
-      ORDER BY invoices.date DESC
-      LIMIT ${ITEMS_PER_PAGE} OFFSET ${offset}
-    `;
-
-    return invoices;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoices.');
+  
+  function parseDailyWeather({ daily }: any): DailytWeatherType[] {
+    return daily.time.map((time: number, index: number) => {
+      return {
+        timestamp: time * 1000, //second to milliseconds
+        iconCode: daily.weather_code[index],
+        maxTemp: Math.round(daily.temperature_2m_max[index]),
+      };
+    });
   }
-}
-
-export async function fetchInvoicesPages(query: string) {
-  try {
-    const data = await sql`SELECT COUNT(*)
-    FROM invoices
-    JOIN customers ON invoices.customer_id = customers.id
-    WHERE
-      customers.name ILIKE ${`%${query}%`} OR
-      customers.email ILIKE ${`%${query}%`} OR
-      invoices.amount::text ILIKE ${`%${query}%`} OR
-      invoices.date::text ILIKE ${`%${query}%`} OR
-      invoices.status ILIKE ${`%${query}%`}
-  `;
-
-    const totalPages = Math.ceil(Number(data[0].count) / ITEMS_PER_PAGE);
-    return totalPages;
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch total number of invoices.');
+  
+  function parseHourlyWeather({ hourly, current }: any): HourlyWeatherType[] {
+    console.log(current.time * 1000);
+    return hourly.time
+      .map((time: number, index: number) => {
+        return {
+          timestamp: time * 1000,
+          iconCode: hourly.weather_code[index],
+          maxTemp: Math.round(hourly.temperature_2m[index]),
+          feelsLike: Math.round(hourly.apparent_temperature[index]),
+          windSpeed: Math.round(hourly.wind_speed_10m[index]),
+          precip: Math.round(hourly.precipitation[index] * 100) / 100,
+        };
+      })
+      .filter(({ timestamp }: any) => timestamp >= current.time * 1000);
   }
-}
-
-export async function fetchInvoiceById(id: string) {
-  try {
-    const data = await sql<InvoiceForm[]>`
-      SELECT
-        invoices.id,
-        invoices.customer_id,
-        invoices.amount,
-        invoices.status
-      FROM invoices
-      WHERE invoices.id = ${id};
-    `;
-
-    const invoice = data.map((invoice) => ({
-      ...invoice,
-      // Convert amount from cents to dollars
-      amount: invoice.amount / 100,
-    }));
-
-    return invoice[0];
-  } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
-  }
-}
-
-export async function fetchCustomers() {
-  try {
-    const customers = await sql<CustomerField[]>`
-      SELECT
-        id,
-        name
-      FROM customers
-      ORDER BY name ASC
-    `;
-
-    return customers;
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Failed to fetch all customers.');
-  }
-}
-
-export async function fetchFilteredCustomers(query: string) {
-  try {
-    const data = await sql<CustomersTableType[]>`
-		SELECT
-		  customers.id,
-		  customers.name,
-		  customers.email,
-		  customers.image_url,
-		  COUNT(invoices.id) AS total_invoices,
-		  SUM(CASE WHEN invoices.status = 'pending' THEN invoices.amount ELSE 0 END) AS total_pending,
-		  SUM(CASE WHEN invoices.status = 'paid' THEN invoices.amount ELSE 0 END) AS total_paid
-		FROM customers
-		LEFT JOIN invoices ON customers.id = invoices.customer_id
-		WHERE
-		  customers.name ILIKE ${`%${query}%`} OR
-        customers.email ILIKE ${`%${query}%`}
-		GROUP BY customers.id, customers.name, customers.email, customers.image_url
-		ORDER BY customers.name ASC
-	  `;
-
-    const customers = data.map((customer) => ({
-      ...customer,
-      total_pending: formatCurrency(customer.total_pending),
-      total_paid: formatCurrency(customer.total_paid),
-    }));
-
-    return customers;
-  } catch (err) {
-    console.error('Database Error:', err);
-    throw new Error('Failed to fetch customer table.');
-  }
-}
