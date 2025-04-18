11e46537e16af6cb8bc5c73571a1e2f9726bce5a
Dermie Madsen <53880481+dermie83@users.noreply.github.com>
2025-04-18 18:53:31 +0100

    Dev 11 (#15)
    * README file updated
    
    * readme file update
    
    * image added
    
    * readme updated
    
    * updates
    
    * updates
    
    * updates to readme
    
    * code clean up
    
    * updated README file
    
    * added amp and updated lighthouse data
    
    * minor page reformating

--- app/home/marine/page.tsx
+++ app/home/marine/page.tsx
@@ -30,19 +30,34 @@ export default async function getServerSideProps(props: {
 
 
   return (
-    <>{error && <ErrorMessage message={errorMessage} />}
-    <h1 className="text-xl md:text-2xl font-bold text-center my-4">Current Marine Forecast</h1>
-    <Search placeholder="Search Lighthouse..."/>
-    <div className="space-y-4">
-      {lighthouses.map(async (lighthouse, index) => {
-        const { current, hourly } = await fetchMarineForecast(lighthouse.latitude, lighthouse.longitude, 'Europe/Dublin');
-
+    <>
+      {error && <ErrorMessage message={errorMessage} />}
+      <h1 className="text-xl md:text-2xl font-bold text-center my-4">Current Marine Forecast</h1>
+        <Search placeholder="Search Lighthouse..."/>
+      <div className="space-y-4">
+        {lighthouses.map(async (lighthouse, index) => {
+          const { current, hourly } = await fetchMarineForecast(lighthouse.latitude, lighthouse.longitude, 'Europe/Dublin');
         return (
-          <div key={lighthouse.name} className="grid grid-cols-1 md:grid-cols-8 grid-rows-3 md:grid-rows-2 gap-4 border-2 shadow-md p-4 md:p-6">
-            <div className="col-span-1 md:col-span-8 row-span-1 text-lg md:text-3xl text-center tracking-wide text-blue-600 dark:text-sky-400">
+        <>
+          <div key={lighthouse.name} className="grid grid-cols-1 md:grid-cols-8 grid-rows-4 md:grid-rows-2 gap-4 border-2 shadow-md p-4 md:p-6">
+            <div className="col-span-1 md:col-span-8 row-span-1 text-lg md:text-3xl text-center tracking-narrow text-blue-600 dark:text-sky-400">
               {lighthouse.name}
             </div>
-            <div className="col-span-1 md:col-span-3 row-span-1">
+            <div className="col-span-1 md:col-span-8 row-span-1">
+              <Header
+                waveHeight={current?.waveHeight}
+                wind_wave_height={current?.wind_wave_height}
+                swell_wave_height={current?.swell_wave_height}
+                wave_direction={current?.wave_direction}
+                wave_period={current?.wave_period}
+                ocean_current_velocity={current?.ocean_current_velocity}
+                ocean_current_direction={current?.ocean_current_direction}
+              />
+            </div>
+            <div className="col-span-1 md:col-span-6 row-span-1">
+              <LineGraph hourly={hourly} />
+            </div>
+            <div className="col-span-1 md:col-span-2 row-span-1">
               <Map
                 key={index}
                 id={lighthouse.id}
@@ -60,30 +75,17 @@ export default async function getServerSideProps(props: {
                 image_url={lighthouse.image_url}
               />
             </div>
-            <div className="col-span-1 md:col-span-5 row-span-1">
-              <Header
-                waveHeight={current?.waveHeight}
-                wind_wave_height={current?.wind_wave_height}
-                swell_wave_height={current?.swell_wave_height}
-                wave_direction={current?.wave_direction}
-                wave_period={current?.wave_period}
-                ocean_current_velocity={current?.ocean_current_velocity}
-                ocean_current_direction={current?.ocean_current_direction}
-              />
-            </div>
-            <div className="col-span-1 md:col-span-8 row-span-1">
-              <LineGraph hourly={hourly} />
-            </div>
           </div>
+        </>
         );
-      })}
-    </div>
-    <div className="mt-5 flex w-full justify-center">
-      <Pagination totalPages={totalPages} />
-    </div>
-    <footer className="w-full bg-gray-800 text-white text-center p-4 mt-4 text-sm md:text-base">
-      <Footer />
-    </footer>
-  </>
+        })}
+      </div>
+      <div className="mt-5 flex w-full justify-center">
+        <Pagination totalPages={totalPages} />
+      </div>
+      <footer className="w-full bg-gray-800 text-white text-center p-4 mt-4 text-sm md:text-base">
+        <Footer />
+      </footer>
+    </>
   )
 }
