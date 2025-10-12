3998540736467f3965d5d06ff47933adb78a9b3f
dermie83 <dermiemadsen@gmail.com>
2025-10-12 18:27:25 +0100

    added a click action to map marker on homepage

--- app/ui/home/map/map.tsx
+++ app/ui/home/map/map.tsx
@@ -1,13 +1,20 @@
+'use client';
+
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
 import {lighthouses} from  "@/app/lib/placeholder-data";
+import { useRouter } from "next/navigation";
 
 //lat: 53.4462988 , lng: -7.5265753
 
 export default function getServerSideProps() {
     const zoom = 7;
+    const router = useRouter();
+    const handleMarkerClick = (lighthouseName: string) => {
+        router.push(`home/forecast?page=1&query=${encodeURIComponent(lighthouseName)}`);
+  };
     return (
         <MapContainer
             center={{lat: 53.4462988 , lng: -7.5265753}}
@@ -20,7 +27,12 @@ export default function getServerSideProps() {
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
         {lighthouses.map((lighthouse)=>
-            <Marker key = {lighthouse.id} position={{lat: lighthouse.latitude , lng: lighthouse.longitude}} draggable={false}>
+            <Marker key = {lighthouse.id} 
+                    position={{lat: lighthouse.latitude , lng: lighthouse.longitude}} 
+                    draggable={false}
+                    eventHandlers={{
+                        click: () => handleMarkerClick(lighthouse.name),
+                    }}>
                 <Popup> 
                     {lighthouse.name}
                 </Popup>
