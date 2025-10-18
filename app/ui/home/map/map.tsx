649480e47ffee4f0bc6d8f3cb3ee43dd14a17516
dermie83 <dermiemadsen@gmail.com>
2025-10-18 12:59:54 +0100

    doubleClick action added to map marker

--- app/ui/home/map/map.tsx
+++ app/ui/home/map/map.tsx
@@ -4,17 +4,23 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css";
 import "leaflet-defaulticon-compatibility";
-import {lighthouses} from  "@/app/lib/placeholder-data";
+import { lighthouses } from  "@/app/lib/placeholder-data";
 import { useRouter } from "next/navigation";
+import { useCallback } from "react";
 
 //lat: 53.4462988 , lng: -7.5265753
 
 export default function getServerSideProps() {
     const zoom = 7;
     const router = useRouter();
-    const handleMarkerClick = (lighthouseName: string) => {
-        router.push(`home/forecast?page=1&query=${encodeURIComponent(lighthouseName)}`);
-  };
+    const handleMarkerDoubleClick = useCallback(
+    (lighthouseName: string) => {
+      router.push(
+        `/home/forecast?page=1&query=${encodeURIComponent(lighthouseName)}`
+      );
+    },
+    [router]
+    );
     return (
         <MapContainer
             center={{lat: 53.4462988 , lng: -7.5265753}}
@@ -31,11 +37,15 @@ export default function getServerSideProps() {
                     position={{lat: lighthouse.latitude , lng: lighthouse.longitude}} 
                     draggable={false}
                     eventHandlers={{
-                        click: () => handleMarkerClick(lighthouse.name),
-                    }}>
-                <Popup> 
-                    {lighthouse.name}
-                </Popup>
+                    // Prevent single click
+                    click: (e) => e.originalEvent.stopPropagation(),
+                    // Handle double click navigation
+                    dblclick: () => handleMarkerDoubleClick(lighthouse.name),
+                    }}
+            >
+            <Popup> 
+                {lighthouse.name}
+            </Popup>
             </Marker>
         )}
         </MapContainer>
