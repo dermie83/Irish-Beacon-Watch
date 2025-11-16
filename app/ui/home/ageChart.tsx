9d1a35836fb4fd5bf3ff3925c583b4f3a0aa0f70
dermie83 <dermiemadsen@gmail.com>
2025-11-16 13:07:45 +0000

    Chart reformat

--- app/ui/home/ageChart.tsx
+++ app/ui/home/ageChart.tsx
@@ -1,16 +1,7 @@
 'use client';
 
-import {
-    CartesianGrid,
-    XAxis,
-    YAxis,
-    Tooltip,
-    BarChart,
-    Bar,
-    Legend,
-    ResponsiveContainer,
-  } from "recharts";
-
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { motion } from 'framer-motion';
 
 type LighthouseTowerProps = {
   ageData: {
@@ -22,18 +13,49 @@ type LighthouseTowerProps = {
 
 export default function AgeGraph({ageData} : LighthouseTowerProps) {
         return (
-          <>
-            <ResponsiveContainer width="100%" minHeight={180}>
-              <BarChart data={ageData}
-                  margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name" />
-                  <YAxis label={{ value: 'Yrs', angle: -90, position: 'upper left', offset: 1000 }}/>
-                  <Tooltip formatter={(value, name) => [`${value} yrs`, name === 'age' ? 'Age':'Age']}/>
-                  <Legend verticalAlign="top" height={36}/>
-                  <Bar dataKey="age" fill="#2399b8" name="Age" />
-              </BarChart>
-            </ResponsiveContainer>
-          </>
+            <div className="w-full h-full p-4 bg-white rounded-2xl shadow-sm">
+              <h2 className="text-xl font-semibold mb-4">
+                Age
+                <span className="text-sm font-medium text-600 ml-1"> - Oldest 15</span>
+              </h2>
+                <motion.div
+                  initial={{ opacity: 0, y: 20 }}
+                  animate={{ opacity: 1, y: 0 }}
+                  transition={{ duration: 0.4 }}
+                  className="w-full h-64">
+                  <ResponsiveContainer width="100%" height="100%">
+                    <BarChart
+                      data={ageData}
+                      margin={{ top: 10, right: 20, left: 10, bottom: 5 }}
+                    >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis
+                      dataKey="name"
+                      angle={-90}
+                      textAnchor="end"
+                      interval={0}
+                      height={90}
+                      tick={{ fontSize: 12, width: 60, dy: 5 }}
+                    />
+                    <YAxis
+                      label={{
+                      value: 'Yrs',
+                      angle: -90,
+                      position: 'insideLeft',
+                      style: { textAnchor: 'middle', fontSize: 12 }
+                      }}
+                    />
+                    <Tooltip formatter={(value) => `${value} yrs`} />
+                    <Legend verticalAlign="top" height={36} />
+                    <Bar
+                      dataKey="age"
+                      name="Age"
+                      fill="#a74f3aff"
+                      radius={[6, 6, 0, 0]}
+                    />
+                    </BarChart>
+                  </ResponsiveContainer>
+                </motion.div>
+            </div>
         )}
  
--- app/ui/home/rangeChart.tsx
+++ app/ui/home/rangeChart.tsx
@@ -1,15 +1,7 @@
 'use client';
 
-import {
-    CartesianGrid,
-    XAxis,
-    YAxis,
-    Tooltip,
-    BarChart,
-    Bar,
-    Legend,
-    ResponsiveContainer,
-  } from "recharts";
+import { ResponsiveContainer, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { motion } from 'framer-motion';
 
 
 type LighthouseRangeProps = {
@@ -22,19 +14,56 @@ type LighthouseRangeProps = {
 
 export default function RangeGraph({rangeData} : LighthouseRangeProps) {
         return (
-          <>
-            <ResponsiveContainer width="100%" minHeight={180}>
-              <BarChart data={rangeData}
-                  margin={{ top: 5, right: 30, left: 20, bottom: 30 }}>
+          <div className="w-full h-full p-4 bg-white rounded-2xl shadow-sm">
+            <h2 className="text-xl font-semibold mb-4">
+              Light Range
+              <span className="text-sm font-medium text-600 ml-1"> - Farthest 15</span>
+            </h2>
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.4 }}
+              className="w-full h-64"
+            >
+              <ResponsiveContainer width="100%" height="100%">
+                <BarChart
+                  data={rangeData}
+                  margin={{ top: 10, right: 20, left: 10, bottom: 5 }}
+                  >
                   <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis dataKey="name"/>
-                  <YAxis label={{ value: 'Nm', angle: -90, position: 'upper left', offset: 100 }}/>
-                  <Tooltip formatter={(value) => [`${value} Nm`]}/>
-                  <Legend verticalAlign="top" height={36}/>
-                  <Bar dataKey="range_w" fill="#8884d8" name="White Light" />
-                  <Bar dataKey="range_r" fill="#8B0000" name="Red Light" />
-              </BarChart>
-            </ResponsiveContainer>
-          </>
+                  <XAxis
+                    dataKey="name"
+                    angle={-90}
+                    textAnchor="end"
+                    interval={0}
+                    height={90}
+                    tick={{ fontSize: 10, width: 60, dy: 5 }}
+                  />
+                  <YAxis
+                    label={{
+                    value: 'Nm',
+                    angle: -90,
+                    position: 'insideLeft',
+                    style: { textAnchor: 'middle', fontSize: 12 }
+                    }}
+                  />
+                  <Tooltip formatter={(value) => `${value} Nm`} />
+                  <Legend verticalAlign="top" height={36} />
+                  <Bar
+                    dataKey="range_w"
+                    name="White Light"
+                    fill="#4C9AFF"
+                    radius={[6, 6, 0, 0]}
+                  />
+                  <Bar
+                    dataKey="range_r"
+                    name="Red Light"
+                    fill="#fa2027ff"
+                    radius={[6, 6, 0, 0]}
+                  />
+                </BarChart>
+              </ResponsiveContainer>
+            </motion.div>
+          </div>
         )}
  
